21cfc8aa705c077b0c38cdadfaa3cd88bfb7596c
Daniel Disowsa <42129480+DroidDaniel@users.noreply.github.com>
2020-05-06 20:15:08 -0700

    Add files via upload

---
+++ js/counter.js
@@ -0,0 +1,34 @@
+/*$(document).ready(function(){
+      $('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
+      });
+*/
+
+$(document).ready(function(){
+$(window).scroll(startCounter);
+function startCounter() {
+    if ($(window).scrollTop() > 200) {
+        $(window).off("scroll", startCounter);
+        $('.count').each(function () {
+            var $this = $(this);
+            jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
+                duration: 1000,
+                easing: 'swing',
+                step: function () {
+                    $this.text(Math.ceil(this.Counter));
+                }
+            });
+        });
+    }
+}
+
+});
