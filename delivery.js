71fd12e9260cdb4903acf1228053da94f867dbe9
Daniel Disowsa <42129480+DroidDaniel@users.noreply.github.com>
2020-05-17 23:05:52 -0700

    Add files via upload

--- delivery.js
+++ delivery.js
@@ -10,3 +10,31 @@ path: 'banneranimation.json'
     
 // Make sure your path has the same filename as your animated     SVG's JSON file //
 })
+
+
+
+
+var animation = bodymovin.loadAnimation({
+container: document.getElementById('deliveryAnimation1'),
+    
+// Set your ID to something that you'll associate with the animation you're using //
+renderer: 'svg',
+loop: true,
+autoplay: true,
+path: 'banneranimation.json'
+    
+// Make sure your path has the same filename as your animated     SVG's JSON file //
+})
+
+
+var animation = bodymovin.loadAnimation({
+container: document.getElementById('deliveryAnimation2'),
+    
+// Set your ID to something that you'll associate with the animation you're using //
+renderer: 'svg',
+loop: true,
+autoplay: true,
+path: 'banneranimation.json'
+    
+// Make sure your path has the same filename as your animated     SVG's JSON file //
+})
